5e9e5f443b81fefb4a44f31365f963836b01571d
mouse <bob.mouse@gmail.com>
2024-06-02 18:28:58 +0100

    added lsp and trying to configure linters

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@ require("remap")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,4 +22,32 @@ vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon
 vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
 
+-- lspconfig
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require'lspconfig'.lua_ls.setup{
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = {'vim'},
+			},
+		},
+	},
+}
+require'lspconfig'.omnisharp.setup{}
+require'lspconfig'.tsserver.setup{}
+
+-- linters
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.csharpier,
+        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.completion.spell,
+    },
+})
 
--- lua/remap/key.lua
+++ lua/remap/key.lua
@@ -1,11 +1,19 @@
 vim.g.mapleader = " "
+vim.wo.number = true
+vim.wo.relativenumber = true
+
 
 vim.keymap.set("n", "Q", "<cmd>qa<cr>")
 
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree filesystem toggle<cr>")
+vim.keymap.set("n", "<leader>pm", vim.cmd.Lazy)
+
+-- window navigation
 vim.keymap.set("n", "<c-h>", "<c-w>hza")
 vim.keymap.set("n", "<c-j>", "<c-w>jza")
 vim.keymap.set("n", "<c-k>", "<c-w>kza")
 vim.keymap.set("n", "<c-l>", "<c-w>lza")
 
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree filesystem toggle<cr>")
-vim.keymap.set("n", "<leader>pm", vim.cmd.Lazy)
+-- buffer navigation
+vim.keymap.set("n", "<c-n>", vim.cmd.bNext)
+vim.keymap.set("n", "<c-p>", vim.cmd.bprevious)
