080d98f5d38afc1d7c5f299308683989bae24dcc
mouse <bob.mouse@gmail.com>
2024-06-04 10:37:32 +0100

    added hop and flash for easy navigation

---
+++ lua/plugins/navigation.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		'phaazon/hop.nvim',
+		-- event = "VeryLazy",
+		branch = 'v2', -- optional but strongly recommended
+		enabled = false,
+		-- lazy = false,
+		keys = {
+			{ "<leader>s",  ":HopChar2<cr>", "Hop Char2" },
+			{ "<leader>S",  ":HopWord<cr>",  "Hop Char2" },
+			{ "<leader>ls", ":HopLine<cr>",  "Hop Line" },
+		},
+		opts = {
+			keys = 'etovxqpdygfblzhckisuran'
+		}
+		-- config = function()
+		-- you can configure Hop the way you like here; see :h hop-config
+		-- require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+
+		-- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+		-- vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+		-- vim.keymap.set('', 'ls', ":HopLine<CR>", { desc = 'Hop Line', silent = true })
+		-- end
+	},
+	{
+		"folke/flash.nvim",
+		-- event = "VeryLazy",
+		---@type Flash.Config
+		opts = {
+			modes = {
+				char = {
+					enabled = false,
+				}
+			}
+		},
+		-- stylua: ignore
+		keys = {
+			{ "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		},
+	}
+}
