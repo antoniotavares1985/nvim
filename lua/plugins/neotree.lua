a70f201ac3a5acca7d76fb51a0822c68b30dc924
TAVARES António <antonio.tavares@capgemini.com>
2024-06-05 15:55:00 +0100

    added some git commands to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,16 +6,32 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
-	config = {
-		close_if_last_window = true,
-		hijack_netrw_behavior = "open_current",
-		window = {
-			position = "right",
-			mapping_options = {
-				noremap = true,
-				nowait = false,
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			enable_git_status = true,
+			hijack_netrw_behavior = "open_current",
+			window = {
+				position = "right",
+				mapping_options = {
+					noremap = true,
+					nowait = false,
+				},
 			},
-		}
-	}
+			git_status = {
+				window = {
+					position = "float",
+					mappings = {
+						["gA"] = "git_add_all",
+						["gu"] = "git_unstage_file",
+						["ga"] = "git_add_file",
+						["gr"] = "git_revert_file",
+						["gc"] = "git_commit",
+						["gp"] = "git_push",
+						["gg"] = "git_commit_and_push",
+					},
+				},
+			},
+		})
+	end,
 }
-
