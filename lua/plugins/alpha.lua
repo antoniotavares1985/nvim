856c3899d22d413589d74f2a4902f6f149076a83
antoniotavares1985 <antonio.tavares@capgemini.com>
2024-06-06 16:15:31 +0100

    using hack nerd font so I changed some icons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -51,14 +51,14 @@ return {
 		local dashboard = require("alpha.themes.dashboard")
 
 		dashboard.section.buttons.val = {
-			dashboard.button("r", " Restore session", "<cmd>SessionRestore<CR>"),
-			dashboard.button("s", " Save Session", "<cmd>SessionSave<cr>"),
+			dashboard.button("r", "󱀸 Restore session", "<cmd>SessionRestore<CR>"),
+			dashboard.button("s", " Save Session", "<cmd>SessionSave<cr>"),
 			dashboard.button("f", " Find file", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("t", " Find text", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("t", " Find text", "<cmd>Telescope live_grep<CR>"),
 			-- dashboard.button('p', ""),
-			dashboard.button("n", " New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("n", " New file", ":ene <BAR> startinsert <CR>"),
 			dashboard.button("o", " Recently opened files", "<cmd>Telescope oldfiles<CR>"),
-			dashboard.button("u", " Update Plugins", "<cmd>Lazy sync<CR>"),
+			dashboard.button("u", "󰚰 Update Plugins", "<cmd>Lazy sync<CR>"),
 			dashboard.button("q", " Quit", ":qa<CR>"),
 		}
 
