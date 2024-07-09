1e259098827e2cbc1298cf8ab00544823a675fc5
antoniotavares1985 <antonio.tavares@capgemini.com>
2024-07-09 20:13:58 +0100

    added autocmd to trigger auto complete

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,11 @@ vim.api.nvim_create_autocmd("VimResized", {
     end,
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client.server_capabilities.hoverProvider then
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
+        end
+    end,
+})
