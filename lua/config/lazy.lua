db37a7b812fed3fca4f46b76b3a91782765e5720
antoniotavares1985 <antonio.tavares@capgemini.com>
2025-05-22 10:53:48 +0100

    removed auto check for plugins update. they are working and I need a working state

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,5 +16,5 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "tokyonight" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false },
 })
